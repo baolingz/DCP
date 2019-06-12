e740cedc2b1a4fc4ceb91a158fdd3a54d3718fcd
Baoling Zhou <baolingz@nyu.edu>
2019-06-12 14:14:27 -0400

    yml

--- csv2html.py
+++ csv2html.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import markdown
 
 # Read the csv file in
 df = pd.read_csv('data_dictionary.csv')
@@ -8,3 +9,5 @@ df.to_html('data_dictionary.html')
 
 # Assign to string
 # htmTable = df.to_html()
+
+# python -m markdown README.md > index.html
